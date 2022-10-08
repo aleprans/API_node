bd42bb50567ea8f3f2c0232c4753a65bf5a60f23
Alexandre Pranskunas <alepranskunas@hotmail.com>
2022-10-08 14:17:47 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const bodyParser = require('body-parser')
-const routes = require('./config/routes')
+const routes = require('./routes')
 
 const app = express()
 
